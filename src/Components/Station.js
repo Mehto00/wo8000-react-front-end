2f367a17d5200b589a64bcbbcd91084ed17b5e93
Mikko Metso <mikkosanterimetso@gmail.com>
2018-02-22 21:00:15 +0200

    Created StationList and Station Components to display fetched data from /latest api end point

--- src/Components/Station.js
+++ src/Components/Station.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import StationList from './StationList';
 
 const Station = props => {
 
-    return (
-		<section className="stations" id="amsterdam">
-			<h2>{props.stationName}</h2>
-			<p>Latest temp: {props.temperature}(&#8451;)<br />
-				<span id="amsterdam-date">{props.timestamps}</span>
-			</p>
-			<p>
-				Max / Min: (past 24h)<br />
-				{props.max} / {props.min}				
-			</p>
-		</section>
+    let observationId = props.observationId
+    let timestamp = props.timestamp
+    let temperature = props.temperature
+    let station = props.station
+    let max = props.max
+    let min = props.min
+    let maxAndMin = max + " / " + min
+
+    if (station !== "new_york") {
+        station = station.charAt(0).toUpperCase() + station.slice(1)
+    } else {
+        let tempStation = station.split("_");
+        tempStation[0] = tempStation[0].charAt(0).toUpperCase() + tempStation[0].slice(1)
+        tempStation[1] = tempStation[1].charAt(0).toUpperCase() + tempStation[1].slice(1)
+        station = tempStation.join(" ");
+    }
+
+    if (max === "null" || min === "null") {
+        maxAndMin = "No observations in the past 24 hours"
+    }
+    
+    return (   
+        <section className="stations">
+            <h2>{station}</h2>
+            <p>Latest temp: {temperature} (&#8451;)<br />
+                <span id="amsterdam-date">{timestamp}</span>
+            </p>
+            <p>
+                Max / Min: (past 24h)<br />
+                {maxAndMin}         
+            </p>
+            
+        </section>
     )
 
 }
 export default Station;
 
-// <span id="amsterdam-max-min">No observations in the past 24 hours</span>
--- src/Components/StationList.js
+++ src/Components/StationList.js
@@ -3,59 +3,23 @@ import Station from './Station';
 
 
 const StationList = props => {
-
-	// const properties = ["observationId", "timestamps", "temperature", "stationName", "max", "min"]
- // 	properties.forEach( function(element, index) {});
-
 	
+	// Saves props.data into a array of objects
+	const results = props.data;
 
-	const results = props.stationData;
-	let listOfStations = [];
-	let station = {
-		timestamps :"",
-		temperature :"",
-		stationName :"",
-		max :"",
-		min :""
-	}
-	if (results != undefined) {
-		station.observationId = results[0];
-		station.timestamps = results[1];
-		station.temperature = results[2];
-		station.stationName = results[3]
-		station.max = results[4]
-		station.min = results[5]
-		listOfStations.push(station);
-	}
-	console.log(station);
-
-		let stations = station.map(temperature => 
- 		<Station temperature={temperature}/>
- 		)
-
-
-	//<Station timestamps={station.timestamps} temperature={station.temperatures} stationName={station.stationName} max={station.max} min={station.min} />
-	  			//<Station temperature={temperatures} />
-	
-
-	//console.log(stations);
-
- // 		let temperatures = results.temperature.map(temperature => 
-	//  		<Station temperature={temperature}/>
-	// 	)
-	//  	let stationName = results.stationName.map(stationName => 
-	// 	 	<Station stationName={stationName}/>
-	// 	)
-	// 	let max = results.max.map(max => 
-	// 	 	<Station max={max}/>
-	// 	)
-	//  	let min = results.min.map(min => 
-	// 	 	<Station min={min}/>
-	//	)
+	//maps results into their own seperate Station 
+	const Stations = ({results}) => (
+	  <div className="stationWrapper">
+	    {results.map(station => (
+	      <Station className="station" key={station.observationId} timestamp={station.timestamp} temperature={station.temperature} station={station.station} max={station.max} min={station.min} />
+	    ))}
+	  </div>
+	); 
     return (
-    	<Station />
-    	//<Station timestamps={station.timestamps} temperature={station.temperature} stationName={station.stationName} max={station.max} min={station.min} />
-	
+		
+		//Renders a list of stations
+    	<Stations results={results} />
+    	
 	)
 }
 
